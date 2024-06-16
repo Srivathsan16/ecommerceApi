9d4aba1806b92cf71343fde4c5454d6e3417e099
srivathsanyara <srivathsan.rajagopal@yara.com>
2024-06-16 22:51:31 +0200

    Test Cases Unit and integration test

--- src/main/java/com/handelsbank/ecommerceApi/EcommerceApiApplication.java
+++ src/main/java/com/handelsbank/ecommerceApi/EcommerceApiApplication.java
@@ -3,6 +3,7 @@ package com.handelsbank.ecommerceApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test commit
 @SpringBootApplication
 public class EcommerceApiApplication {
 
