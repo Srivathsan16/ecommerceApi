4f1dab2903a2a98450a8f426bf8783257f959245
srivathsanyara <srivathsan.rajagopal@yara.com>
2024-06-17 09:21:07 +0200

    Some fixes

--- src/test/java/com/handelsbank/ecommerceApi/integration/IntegrationTests.java
+++ src/test/java/com/handelsbank/ecommerceApi/integration/IntegrationTests.java
@@ -1,10 +1,7 @@
-/*
+
 package com.handelsbank.ecommerceApi.integration;
 
 
-
-import com.handelsbank.ecommerceApi.entities.DiscountEntity;
-import com.handelsbank.ecommerceApi.entities.ProductEntity;
 import com.handelsbank.ecommerceApi.repository.DiscountRepository;
 import com.handelsbank.ecommerceApi.repository.ProductRepository;
 import com.handelsbank.ecommerceApi.services.CheckoutService;
@@ -30,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IntegrationTests {
 
     @Container
-    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.26")
+    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("testdb")
             .withUsername("test")
             .withPassword("test");
@@ -38,37 +35,17 @@ public class IntegrationTests {
     @Autowired
     private CheckoutService checkoutService;
 
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private DiscountRepository discountRepository;
 
     @BeforeEach
     void setup() {
-        ProductEntity product1 = new ProductEntity();
-        product1.setName("Product 1");
-        product1.setPrice(new BigDecimal("100.00"));
-        productRepository.save(product1);
-
-        ProductEntity product2 = new ProductEntity();
-        product2.setName("Product 2");
-        product2.setPrice(new BigDecimal("200.00"));
-        productRepository.save(product2);
-
-        DiscountEntity discount1 = new DiscountEntity();
-        discount1.setProduct(product1);
-        discount1.setDiscountedPrice(new BigDecimal("90.00"));
-        discount1.setQuantityRequired(2);
-        discountRepository.save(discount1);
     }
 
     @Test
     public void testCheckoutProcessSuccess() {
-        List<Long> productIds = Arrays.asList(1L, 1L, 2L);
+        List<Long> productIds = Arrays.asList(1L, 1L,1L, 2L);
         BigDecimal total = checkoutService.calculateTotalPrice(productIds);
         assertNotNull(total);
-        assertEquals(0, total.compareTo(new BigDecimal("390.00")), "Total price calculated is incorrect.");
+        assertEquals(0, total.compareTo(new BigDecimal("280.00")), "Total price calculated is incorrect.");
     }
 
     @Test
@@ -80,5 +57,5 @@ public class IntegrationTests {
         assertTrue(exception.getMessage().contains("not found"));
     }
 }
-*/
+
 
